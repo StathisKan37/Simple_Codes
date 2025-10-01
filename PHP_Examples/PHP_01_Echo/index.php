23b9ea722f819fefea90fc37ee73e4b7a50f9019
Stathis <138621887+StathisKan37@users.noreply.github.com>
2025-10-01 19:09:05 +0300

    Add files via upload

---
+++ PHP_Examples/PHP_01_Echo/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+	// Printing the phrase 'Hello World!'
+	echo '<h1>Hello World!</h1>';
+	
+	// Saving 2 strings in variables
+	$my_name = 'Stathis';
+	$my_last_name = 'Kan 37';
+	
+	// Printing the variables
+	echo "<h3>My name is {$my_name}</h3>";
+	echo '<h3>And '. $my_last_name.' is my last name</h3>'
+?>
+</body>
+</html>
