7f225b06989f944a33ffbf8a5dbdaff0b67909cd
Stathis <138621887+StathisKan37@users.noreply.github.com>
2025-10-15 12:13:54 -0400

    Delete PHP_Examples directory

--- PHP_Examples/PHP_10_Request_GET.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<body>
-
-<!-- This link sends GET parameters (name and age) in the URL -->
-<a href="Other_files/PHP_File_request_get.php?name=StathisKan37&age=24">GET Request</a>
-
-</body>
-</html>
